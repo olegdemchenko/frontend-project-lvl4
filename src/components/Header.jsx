5aa00b8c1f6f8ac94dd99d76e5a1eca44aee514a
odemch <odemch@softserveinc.com>
2021-12-01 16:01:36 +0200

    Fix redirecting after click on header logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,9 +9,9 @@ export default () => {
   return (
     <Navbar className="shadow-sm" variant="light" bg="white" expand="lg">
       <Container>
-        <Link className="navbar-brand" to="/login">React Chat</Link>
+        <Link className="navbar-brand" to="/">React Chat</Link>
         { loggedIn && <Button className onClick={logOut} variant="primary">Logout</Button> }
       </Container>
     </Navbar>
   );
-}
+};
