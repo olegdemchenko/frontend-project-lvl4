1b437d3721ad666c44223495a33cdf197abfced5
odemch <odemch@softserveinc.com>
2021-12-03 13:03:03 +0200

    Change Rollbar connection implementation

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -7,11 +7,16 @@ const mode = process.env.NODE_ENV || 'development';
 
 module.exports = {
   mode,
+  entry: {
+    main: './src/index.js',
+    rollbar: './src/rollbar.js',
+  },
   resolve: {
     extensions: ['.js', '.jsx'],
   },
   output: {
     path: path.join(__dirname, 'dist', 'public'),
+    filename: '[name].js',
     publicPath: '/assets/',
   },
   devServer: {
