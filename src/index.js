4100bd7f032744be62480262d1d3d393897601a5
odemch <odemch@softserveinc.com>
2021-12-07 16:08:05 +0200

    Create containers for app render if not existed

--- src/index.js
+++ src/index.js
@@ -8,6 +8,22 @@ if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
+const chat = document.querySelector('#chat');
+if (!chat) {
+  document.body.classList.add('h-100', 'bg-light');
+  const firstContainer = document.createElement('div');
+  firstContainer.classList.add('h-100');
+  document.body.append(firstContainer);
+  const newChat = document.createElement('div');
+  chat.setAttribute('id', 'chat');
+  chat.classList.add('h-100');
+  firstContainer.append(newChat);
+  const chatContainer = document.createElement('div');
+  chatContainer.setAttribute('id', 'chat-container');
+  chatContainer.classList.add('d-flex', 'flex-column', 'h-100');
+  chat.append(chatContainer);
+}
+
 initApp();
 
 export default initApp;
