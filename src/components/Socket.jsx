3b686e6402d3c4f64cc88292f6d3c44c3748e1ba
odemch <odemch@softserveinc.com>
2021-12-10 18:24:51 +0200

    Retrieve socket functionality into custom hook

--- src/components/Socket.jsx
+++ src/components/Socket.jsx
@@ -1,64 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import { Alert } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import SocketContext from '../contexts/SocketContext';
-import {
-  setStatus as setChatStatus,
-} from '../store/chatSlice';
-import {
-  addMessage,
-  setStatus as setMessagesStatus,
-  deleteChannelMessages,
-} from '../store/messagesSlice';
-import {
-  addChannel,
-  setStatus as setChannelsStatus,
-  renameChannel,
-  deleteChannel,
-  setCurrentChannel,
-  setDefaultChannel,
-} from '../store/channelsSlice';
+import useSocket from '../hooks/useSocket';
 
 const Socket = ({ socket, children }) => {
   const [error, setError] = useState(null);
   const [notification, notify] = useState(null);
-  const dispatch = useDispatch();
+  const socketFunctions = useSocket(socket, notify, setError);
   const { t } = useTranslation();
-
-  useEffect(() => {
-    socket.on('connect', () => {
-      console.log('websocket has been connected!');
-    });
-    socket.on('connect_error', () => {
-      console.log('connection error');
-      setError(t('socket.errors.connectionErr'));
-    });
-    socket.on('newMessage', (message) => {
-      dispatch(addMessage(message));
-    });
-    socket.on('newChannel', (channel) => {
-      dispatch(addChannel(channel));
-      dispatch(setCurrentChannel(channel.id));
-      notify('addChannelSuccess');
-    });
-    socket.on('renameChannel', (channel) => {
-      dispatch(renameChannel(channel));
-      notify('renameChannelSuccess');
-    });
-    socket.on('removeChannel', (channel) => {
-      dispatch(deleteChannel(channel));
-      dispatch(deleteChannelMessages(channel));
-      dispatch(setDefaultChannel());
-      notify('removeChannelSuccess');
-    });
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
 
   const notificationsMap = {
     addChannelSuccess: t('chat.addChannelSuccess'),
@@ -85,62 +38,12 @@ const Socket = ({ socket, children }) => {
     }
   }, [notification]);
 
-  const sendMessage = (message) => {
-    dispatch(setMessagesStatus('sending'));
-    socket.emit('newMessage', message, ({ status }) => {
-      if (status === 'ok') {
-        dispatch(setMessagesStatus('sendingSuccess'));
-      } else {
-        setChatStatus('sendMessageError');
-      }
-    });
-  };
-
-  const createChannel = (channel) => {
-    dispatch(setChannelsStatus('sending'));
-    socket.emit('newChannel', channel, ({ status }) => {
-      if (status === 'ok') {
-        dispatch(setChannelsStatus('sendingSuccess'));
-      } else {
-        setChatStatus('createChannelError');
-      }
-    });
-  };
-
-  const changeChannelName = (channelData) => {
-    dispatch(setChannelsStatus('sending'));
-    socket.emit('renameChannel', channelData, ({ status }) => {
-      if (status === 'ok') {
-        dispatch(setChannelsStatus('sendingSuccess'));
-      } else {
-        setChatStatus('renameChannelError');
-      }
-    });
-  };
-
-  const removeChannel = (channel) => {
-    dispatch(setChannelsStatus('sending'));
-    socket.emit('removeChannel', channel, ({ status }) => {
-      if (status === 'ok') {
-        dispatch(setChannelsStatus('sendingSuccess'));
-      } else {
-        setChatStatus('removeChannelError');
-      }
-    });
-  };
-
   if (error) {
     return <Alert variant="danger">{`Error: ${error}`}</Alert>;
   }
 
   return (
-    <SocketContext.Provider value={{
-      sendMessage,
-      createChannel,
-      changeChannelName,
-      removeChannel,
-    }}
-    >
+    <SocketContext.Provider value={{ ...socketFunctions }}>
       {children}
       <ToastContainer />
     </SocketContext.Provider>
---
+++ src/hooks/useSocket.js
@@ -0,0 +1,107 @@
+import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
+
+import {
+  setStatus as setChatStatus,
+} from '../store/chatSlice';
+import {
+  addMessage,
+  setStatus as setMessagesStatus,
+  deleteChannelMessages,
+} from '../store/messagesSlice';
+import {
+  addChannel,
+  setStatus as setChannelsStatus,
+  renameChannel,
+  deleteChannel,
+  setCurrentChannel,
+  setDefaultChannel,
+} from '../store/channelsSlice';
+
+const useSocket = (socket, notify, setError) => {
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('websocket has been connected!');
+    });
+    socket.on('connect_error', () => {
+      console.log('connection error');
+      setError(t('socket.errors.connectionErr'));
+    });
+    socket.on('newMessage', (message) => {
+      dispatch(addMessage(message));
+    });
+    socket.on('newChannel', (channel) => {
+      dispatch(addChannel(channel));
+      dispatch(setCurrentChannel(channel.id));
+      notify('addChannelSuccess');
+    });
+    socket.on('renameChannel', (channel) => {
+      dispatch(renameChannel(channel));
+      notify('renameChannelSuccess');
+    });
+    socket.on('removeChannel', (channel) => {
+      dispatch(deleteChannel(channel));
+      dispatch(deleteChannelMessages(channel));
+      dispatch(setDefaultChannel());
+      notify('removeChannelSuccess');
+    });
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+  const sendMessage = (message) => {
+    dispatch(setMessagesStatus('sending'));
+    socket.emit('newMessage', message, ({ status }) => {
+      if (status === 'ok') {
+        dispatch(setMessagesStatus('sendingSuccess'));
+      } else {
+        setChatStatus('sendMessageError');
+      }
+    });
+  };
+
+  const createChannel = (channel) => {
+    dispatch(setChannelsStatus('sending'));
+    socket.emit('newChannel', channel, ({ status }) => {
+      if (status === 'ok') {
+        dispatch(setChannelsStatus('sendingSuccess'));
+      } else {
+        setChatStatus('createChannelError');
+      }
+    });
+  };
+
+  const changeChannelName = (channelData) => {
+    dispatch(setChannelsStatus('sending'));
+    socket.emit('renameChannel', channelData, ({ status }) => {
+      if (status === 'ok') {
+        dispatch(setChannelsStatus('sendingSuccess'));
+      } else {
+        setChatStatus('renameChannelError');
+      }
+    });
+  };
+
+  const removeChannel = (channel) => {
+    dispatch(setChannelsStatus('sending'));
+    socket.emit('removeChannel', channel, ({ status }) => {
+      if (status === 'ok') {
+        dispatch(setChannelsStatus('sendingSuccess'));
+      } else {
+        setChatStatus('removeChannelError');
+      }
+    });
+  };
+  return {
+    sendMessage,
+    createChannel,
+    changeChannelName,
+    removeChannel,
+  };
+};
+
+export default useSocket;
